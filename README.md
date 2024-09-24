150a239d500e11acfe918fc5d67e180673b1dc9b
JeodC <47716344+JeodC@users.noreply.github.com>
2024-09-24 16:46:53 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## Installation
+Purchase the game on Steam and using the steam console, do `download_depot 1147860 1147861 6512411704625720880`.
+
+Copy all the data to `ports/ufo50/gamedata`. On first run the game will be patched.
+
+## Performance Tweaks
+In the game options set scale to 1 for full speed.
+
+## Thanks
+Mossmouth -- The absolutely amazing game  
+JohnnyOnFlame -- GMLoader-Next and TextureRepacker via UTMT  
+Cyril aka kotzebuedog -- GMTools audio patcher  
+Testers & Devs from the PortMaster Discord
---
+++ UFO 50.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
+
+if [ -d "/opt/system/Tools/PortMaster/" ]; then
+  controlfolder="/opt/system/Tools/PortMaster"
+elif [ -d "/opt/tools/PortMaster/" ]; then
+  controlfolder="/opt/tools/PortMaster"
+elif [ -d "$XDG_DATA_HOME/PortMaster/" ]; then
+  controlfolder="$XDG_DATA_HOME/PortMaster"
+else
+  controlfolder="/roms/ports/PortMaster"
+fi
+
+source $controlfolder/control.txt
+source $controlfolder/device_info.txt
+[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
+get_controls
+
+# Setup permissions
+$ESUDO chmod 666 /dev/tty1
+$ESUDO chmod 666 /dev/uinput
+echo "Loading, please wait... (might take a while!)" > $CUR_TTY
+
+# Variables
+GAMEDIR="/$directory/ports/ufo50"
+
+# CD and set permissions
+cd $GAMEDIR
+> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
+$ESUDO chmod +x -R $GAMEDIR/*
+
+# Exports
+export LD_LIBRARY_PATH="$GAMEDIR/libs:$GAMEDIR/tools/lib:$LD_LIBRARY_PATH"
+export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
+export TOOLDIR="$GAMEDIR/tools"
+export TMPDIR="$GAMEDIR/tmp"
+export PATH="$GAMEDIR/tools:$PATH"
+
+# Check if patchlog.txt to skip patching
+if [ ! -f patchlog.txt ]; then
+    $GPTOKEYB "love" &
+    ./love patcher -f "tools/patchscript" -g "UFO 50" -t "2 to 5 minutes"
+    $ESUDO kill -9 $(pidof gptokeyb)
+else
+    echo "Patching process already completed. Skipping."
+fi
+
+# Gmloader with conditional command
+if [[ "$CFW_NAME" == "TrimUI" ]]; then
+    $GPTOKEYB "gmloadernext" -c "./ufo50.gptk" & 
+else
+    $GPTOKEYB "gmloadernext" & 
+fi
+
+./gmloadernext game.apk
+
+# Kill processes
+$ESUDO kill -9 $(pidof gptokeyb)
+$ESUDO systemctl restart oga_events &
+printf "\033c" > /dev/tty0
